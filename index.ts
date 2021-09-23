f3887ef6e68692ee13f6db91788375445f980fc2
Alexandra <alexandrapigulevskaya@gmail.com>
2021-09-23 22:51:22 +0300

    UPD

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ import ChatPage from './src/pages/chat/chat.ts';
 let path = window.location.pathname;
 let page;
 
+console.log('path', path)
+
 function render(query: string, block: string) {
   const root = document.querySelector(query);
   root.appendChild(block.getContent());
